d3aa797e59595bb50a4d0023191014fc490f9f9a
Vênus <58833700+zilohnoji@users.noreply.github.com>
2023-01-21 00:04:24 -0300

    Add files via upload

--- cs50/population.c
+++ cs50/population.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int live;
+    int end, counter = 0;
+
+    do
+    {
+        printf("\nStart: ");
+        scanf("%i", &live);
+
+        printf("End: ");
+        scanf("%i", &end);
+    } while ((live < 9) || end < live);
+
+    while (live < end)
+    {
+        live += (live / 3) - (live / 4);
+        counter++;
+    }
+
+    printf("Years: %i", counter);
+}
